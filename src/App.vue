bcd9ab154c0859836380ca4355f4ddf1911b8177
linainu <voropaeva.polina.s@gmail.com>
2021-07-28 17:45:40 +0300

    feat: Make fb auth functions

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,6 @@ export default {
   },
   components: {
     EmptyLayout, MainLayout
-  }
+  },
 }
 </script>
---
+++ src/store/auth.js
@@ -0,0 +1,33 @@
+import db from '../firebaseInit'
+import firebase from 'firebase'
+
+export default {
+    actions: {
+        async login({commit}, {email, password}) {
+            try {
+                await firebase.auth().signInWithEmailAndPassword(email, password)
+            } catch(e) {
+              console.log(e)
+            }
+        },
+        async logout({commit}) {
+            await firebase.auth().signOut()
+        },
+        // регистрация пользователя
+        async register({dispatch, commit}, {email, password, name}) {
+            try {
+                await firebase.auth().createUserWithEmailAndPassword(email, password)
+                const uid = await dispatch('getUid')
+                await db.collection('users').doc(uid).set({name})
+            } catch(e) { 
+               console.log(e)
+            }
+        },
+        getUid() {
+            const user = firebase.auth().currentUser
+            return user ? user.uid : null
+        }
+    },
+    
+}
+
