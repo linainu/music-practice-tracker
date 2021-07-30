e2f7551264f2b8f1206c72d5abd4fad271689b70
linainu <voropaeva.polina.s@gmail.com>
2021-07-30 17:14:11 +0300

    feat: Add date filter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+import dateFilter from '@/filters/date.filter'
+
+Vue.filter('date', dateFilter)
+
 Vue.config.productionTip = false
 
 let app
