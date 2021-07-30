01313aaf36bcfdbfa8f77dce4090e05b245b68f7
linainu <voropaeva.polina.s@gmail.com>
2021-07-30 14:47:05 +0300

    feat: Add the error bus

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import songs from './songs'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    error: null
   },
   mutations: {
+    setError(state, error) {
+      state.error = error
+    },
+    clearError(state) {
+      state.error = null
+    }
   },
   actions: {
   },
   modules: {
-    auth
+    auth, songs
   },
   getters: {
+    error: s => s.error
   }
 })
